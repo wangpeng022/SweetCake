e79460c9c7f23d0c0b625e554083b0dd15d786b4
chloe-huang <2502929029@qq.com>
2017-11-23 21:45:30 +0800

    tools

--- src/containers/Profile/Collect/collect.js
+++ src/containers/Profile/Collect/collect.js
@@ -1,7 +1,12 @@
 import React,{Component} from 'react';
 import './collect.less';
+import {connect} from 'react-redux'
+import actions from '../../../store/actions/session'
+ class Collect extends Component{
 
-export default class Collect extends Component{
+    componentDidMount(){
+        this.props.getCollections();
+    }
     render(){
         return (
             <div className="collect-top">
@@ -15,10 +20,27 @@ export default class Collect extends Component{
                 </a>
 
                 {/*收藏列表*/}
+                {/*<ul className="collect-lists">*/}
+                    {/*<li>*/}
+                        {/*<a href="">*/}
+                            {/*<div className="collect-img"></div>*/}
+                            {/*<div className="background"/>*/}
+                        {/*</a>*/}
+                        {/*<div className="collect-tittle">*/}
+                            {/*<p className="big-title">咸蛋黄肉松贝果</p>*/}
+                            {/*<p className="small-title">面包</p>*/}
+                        {/*</div>*/}
+                        {/*<div className="collect-data">*/}
+                            {/*收藏 9*/}
+                            {/*<span>·</span>*/}
+                            {/*评论 7*/}
+                        {/*</div>*/}
+                    {/*</li>*/}
+                {/*</ul>*/}
                 <ul className="collect-lists">
                     <li>
                         <a href="">
-                            <div className="collect-img"></div>
+                            <div className="collect-img"/>
                             <div className="background"/>
                         </a>
                         <div className="collect-tittle">
@@ -36,3 +58,8 @@ export default class Collect extends Component{
         )
     }
 }
+
+export default connect(
+    state=>state.session,
+    actions
+)(Collect);
--- src/containers/Tools/tools.js
+++ src/containers/Tools/tools.js
@@ -1,11 +1,81 @@
 import React,{Component} from 'react';
-
+import './tools.less'
+import {Link} from 'react-router-dom'
 export default class Tools extends Component{
     render(){
         return (
-            <div>
-                tools
-            </div>
+          <div className="tools-body">
+              <div className="tools-top">
+                  <div className="left"><Link to={'/home'} className="back"><i>&lt;</i></Link></div>
+                  <div className="tools-middle">工具</div>
+              </div>
+
+              {/*转换器*/}
+              <div className="props-conversion">
+                  <div className="propsName">
+                      <i>X</i>
+                      <span>道具转换器</span>
+                  </div>
+
+                  <div className="content">
+                      {/*左边转换器*/}
+                      <div className="left">
+                          <div className="prop_border">
+                              <div className="box">
+                                  <input type="hidden" className="t"/>
+                                  <input type="hidden" className="area"/>
+                                  <div className="size">
+                                      <span className="X">8</span>
+                                      X
+                                      <span className="Y">8</span>
+                                      <span className="unit">厘米</span>
+                                  </div>
+                                  <div className="border">
+                                      <i className="cure"/>
+                                  </div>
+                                  <div className="number">
+                                      <span className="num">1</span>
+                                      个
+                                  </div>
+
+                              </div>
+                          </div>
+                      </div>
+
+                      {/*右边转换器*/}
+                      <div className="right">
+                          <div className="prop_border">
+                              <div className="box">
+                                  <input type="hidden" className="t"/>
+                                  <input type="hidden" className="area"/>
+                                  <div className="size">
+                                      <span className="Y">8</span>
+                                      <span className="unit">厘米</span>
+                                  </div>
+                                  <div className="border">
+                                      <i className="cure"/>
+                                  </div>
+                                  <div className="number">
+                                      <span className="num">1</span>
+                                      个
+                                  </div>
+
+                              </div>
+                          </div>
+                      </div>
+
+                      {/*相互转换按钮*/}
+                      <div className="switch">
+                          <i className="btn"/>
+                      </div>
+
+                      {/*转换箭头*/}
+                      <div className="arrow">
+                          <i className="change"/>
+                      </div>
+                  </div>
+              </div>
+          </div>
         )
     }
 }
