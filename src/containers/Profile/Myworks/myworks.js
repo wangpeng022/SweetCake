e942a0a62b8b5eaad17a2fa06d470104c6e62254
Lookkkk <1979790369@qq.com>
2017-11-24 23:09:11 +0800

    修改works

--- src/containers/Profile/Myworks/myworks.js
+++ src/containers/Profile/Myworks/myworks.js
@@ -2,30 +2,64 @@ import React, {Component} from 'react';
 import './myworks.less'
 import {connect} from 'react-redux';
 import actions from '../../../store/actions/works';
+import {CSSTransition} from 'react-transition-group';
 
-
+const Fade = ({children, ...props}) => {
+    console.log({...props});
+    return (
+        <CSSTransition
+            {...props}
+            timeout={1200}
+            classNames="fade"
+        >
+            {children}
+        </CSSTransition>
+    )
+};
 
 class Myworks extends Component {
-    constructor(){
+    constructor() {
         super();
-        this.state={
-            works:[]
+        this.state = {
+            works: [],
+            show: false,
         }
     }
+
+    handleClose = (e) => {
+        e.preventDefault();
+        if (this.state.show) {
+            setTimeout(()=>this.$div.style.display = 'none',500);
+            this.setState({show: !this.state.show});
+            this.$div.style.opacity = 0.01;
+        }
+        /*this.$div.style.display = 'none';
+        this.setState({show: !this.state.show});
+        this.$div.style.opacity = 0.01;*/
+    };
+    handleClick = (e) => {
+        console.log('xxxxxxxxxxxxxxxxxxxxxxxxx');
+        e.preventDefault();
+        this.$div.style.display = 'block';
+        setTimeout(() => this.$div.style.opacity = 1);
+        this.setState({show: !this.state.show});
+    };
+
     componentDidMount() {
         let user = JSON.parse(localStorage.getItem('user')).id;
         console.log(user);
         this.props.getUser(user);
     }
 
-    componentWillReceiveProps(props){
+    componentWillReceiveProps(props) {
         console.log(props);
-        this.setState({works:props.works})
+        this.setState({works: props.works});
     }
+
     render() {
         console.log(this.props);
         return (
-            <div className="works-top">
+            <div className="works-top" onClick={this.handleClose}>
                 <div className="left" onClick={() => this.props.history.goBack()}><a href="#"
                                                                                      className="back"><i>&lt;</i></a>
                 </div>
@@ -33,21 +67,40 @@ class Myworks extends Component {
 
                 <div className="list-works">
                     <ul>
-                        {this.state.works.map((item,index)=>{
-                            return (
+                        {this.state.works.map((item, index) => (
                                 <li key={index} className="list">
                                     <div className="item">
                                         <a href="">
                                             <div className="picture">
-                                                <img src={item.url} className="img"/>
+                                                <div onClick={this.handleClick}
+                                                     style={{backgroundImage: `url(${item.url})`}} className="img"/>
                                             </div>
                                         </a>
                                     </div>
+                                    <Fade in={this.state.show}>
+                                        <div ref={div => this.$div = div} className="works-big">
+                                            <div className="works-left">
+                                                <img src={this.props.user_img}></img>
+                                                <span>{this.props.author}</span>
+                                            </div>
+                                            <div className="works-right">
+                                                {this.props.create_time}
+                                            </div>
+                                            <div className="works-img">
+                                                <img src='http://beile.bakelulu.com.cn/Fgj9OXtQTuPMRzUev0FUCv1CE-3f'
+                                                     alt=""/>
+                                            </div>
+                                            <div className="works-footer">
+                                                <div className="works-footer1">{item.title}</div>
+                                                <div className="works-footer2">{item.describe}</div>
+                                            </div>
+                                        </div>
+                                    </Fade>
                                 </li>
                             )
-                        })}
+                        )}
                     </ul>
-                    <p className="end">没有更多作品了~</p>
+                    <p onClick={this.handleClose} className="end">没有更多作品了~</p>
                 </div>
             </div>
         )
