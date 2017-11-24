b7a2811e970f9806e2c4b6c452a4f4db555cc615
Lookkkk <1979790369@qq.com>
2017-11-24 20:57:55 +0800

    LooKKKK

--- src/containers/Profile/Myworks/myworks.js
+++ src/containers/Profile/Myworks/myworks.js
@@ -1,32 +1,51 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import './myworks.less'
-export default class Myworks extends Component{
-    render(){
+import {connect} from 'react-redux';
+import actions from '../../../store/actions/works';
+
+
+
+class Myworks extends Component {
+    constructor(){
+        super();
+        this.state={
+            works:[]
+        }
+    }
+    componentDidMount() {
+        let user = JSON.parse(localStorage.getItem('user')).id;
+        console.log(user);
+        this.props.getUser(user);
+    }
+
+    componentWillReceiveProps(props){
+        console.log(props);
+        this.setState({works:props.works})
+    }
+    render() {
+        console.log(this.props);
         return (
             <div className="works-top">
-                <div className="left" onClick={()=>this.props.history.goBack()}><a href="#" className="back"><i>&lt;</i></a></div>
+                <div className="left" onClick={() => this.props.history.goBack()}><a href="#"
+                                                                                     className="back"><i>&lt;</i></a>
+                </div>
                 <div className="collect-middle">我的作品</div>
 
                 <div className="list-works">
                     <ul>
-                        <li className="list">
-                            <div className="item">
-                                <a href="">
-                                <div className="picture">
-                                    <div className="img"/>
-                                </div>
-                            </a>
-                            </div>
-                        </li>
-                        <li className="list">
-                            <div className="item">
-                                <a href="">
-                                    <div className="picture">
-                                        <div className="img"/>
+                        {this.state.works.map((item,index)=>{
+                            return (
+                                <li key={index} className="list">
+                                    <div className="item">
+                                        <a href="">
+                                            <div className="picture">
+                                                <img src={item.url} className="img"/>
+                                            </div>
+                                        </a>
                                     </div>
-                                </a>
-                            </div>
-                        </li>
+                                </li>
+                            )
+                        })}
                     </ul>
                     <p className="end">没有更多作品了~</p>
                 </div>
@@ -34,3 +53,5 @@ export default class Myworks extends Component{
         )
     }
 }
+
+export default connect(state => state.user, actions)(Myworks);
--- src/store/actions/works.js
+++ src/store/actions/works.js
@@ -1,5 +1,5 @@
 import * as types from '../action-types'
-import {postWorks,postDraft} from '../../api/works'
+import {postWorks, postDraft, getUser} from '../../api/works'
 import {push} from 'react-router-redux'
 
 export default {
@@ -20,6 +20,16 @@ export default {
                     type: types.POST_DRAFT,
                     payload
                 });
+            })
+        }
+    },
+    getUser(data) {
+        return dispatch => {
+            getUser(data).then(payload => {
+                dispatch({
+                    type: types.GET_USER,
+                    payload
+                })
             })
         }
     }
